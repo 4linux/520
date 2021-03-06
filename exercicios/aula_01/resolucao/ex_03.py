e566ce9e6c51f4026452ba3d0f75f5abba5844aa
Guilherme Zanelato <guilherme.zanelato@gmail.com>
2021-03-05 21:07:56 -0300

    add: adicionado resolução do exercício 3
    Adicionado a resolução do exercício 3 do primeiro capítulo do curso
    Python Fundamentals

---
+++ exercicios/aula_01/resolucao/ex_03.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#1: Capturar dois números
+
+n1 = int(input('N1: '))
+n2 = int(input('N2: '))
+
+#2: realizar a soma dos dois números
+soma = n1 + n2
+
+#3: realizar a diferença dos dois números
+dif = n1 - n2 
+
+#4: apresentar o resultado conforme solicitado
+print('--------------------------------')
+print(f'Soma: {n1} + {n2} = {soma}')
+print(f'Diferença: {n1} - {n2} = {dif}')
+
+
