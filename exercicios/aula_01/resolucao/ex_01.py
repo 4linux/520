4f6931bcdcd10e19f9ab871bc6ab19605de185ea
Guilherme Zanelato <guilherme.zanelato@gmail.com>
2021-03-05 21:00:29 -0300

    add: adicionado resolução do exercício 01
    Foi adicionado a resolução do primeiro exercício do capítulo 1 da
    apostila.

---
+++ exercicios/aula_01/resolucao/ex_01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3 
+
+#1: captura as informações de acordo com o enunciado
+nome = input('Digite o seu nome: ')
+cpf = input('Digite o seu CPF: ')
+idade = input('Digite a sua idade: ')
+pais = input('País: ')
+
+#2: apresentação dos dados de acordo com o enunciado
+print('------------------------------')
+print('Confirmação de cadastro')
+print(f'Nome: {nome}')
+print(f'CPF : {cpf}')
+print(f'Idade: {idade}')
+print(f'País: {pais}')
+print('------------------------------')
+
